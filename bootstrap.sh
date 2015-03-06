d0d6a59adcf49e5a45d8e7eedbe889611236dc7d
jstumbaugh <jstumbaugh@smu.edu>
2015-03-06 14:38:14 -0600

    got vagrant file and shell script working

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,18 @@
 
 apt-get update
 apt-get install -y apache2
+
 if ![-L /var/www]; then
 	rm -rf /var/www
 	ln -fs /vagrant /var/www
 fi
 
-apt-get install -y lamp-server^
+#apt-get install -y lamp-server^
 
-libapache2-mod-php5
-sudo a2enmod php5
-sudo service apache2 restart
-mysql-server libapache2-mod-auth-mysql php5-mysql
-mysql -u root
+#will set password of mysql to "mysqlpassword"
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysqlpassword'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysqlpassword'
+sudo apt-get -y install mysql-server
+
+# to install PHP
+sudo apt-get install php5 -y libapache2-mod-php5 php5-mcrypt
